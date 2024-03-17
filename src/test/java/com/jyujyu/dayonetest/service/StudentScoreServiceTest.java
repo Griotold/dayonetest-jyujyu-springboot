519d7be3928e4fcbed8e5406b67a1428bcc42688
Griotold <gotjd9773@naver.com>
2024-03-17 19:18:34 +0900

    합격자 명단, 불합격자 명단 가져오기 - Mockito 로 데이터베이스가 이러한 데이터를 Return 한다고 지정해줄 수 있다.

--- src/test/java/com/jyujyu/dayonetest/service/StudentScoreServiceTest.java
+++ src/test/java/com/jyujyu/dayonetest/service/StudentScoreServiceTest.java
@@ -1,5 +1,9 @@
 package com.jyujyu.dayonetest.service;
 
+import com.jyujyu.dayonetest.controller.response.ExamFailStudentResponse;
+import com.jyujyu.dayonetest.controller.response.ExamPassStudentResponse;
+import com.jyujyu.dayonetest.model.StudentFail;
+import com.jyujyu.dayonetest.model.StudentPass;
 import com.jyujyu.dayonetest.repository.StudentFailRepository;
 import com.jyujyu.dayonetest.repository.StudentPassRepository;
 import com.jyujyu.dayonetest.repository.StudentScoreRepository;
@@ -7,7 +11,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class StudentScoreServiceTest {
 
@@ -106,4 +113,91 @@ class StudentScoreServiceTest {
         Mockito.verify(studentFailRepository, Mockito.times(1)).save(Mockito.any());
     }
 
+    @Test
+    @DisplayName("합격자 명단 가져오기 검증")
+    void getPassStudentsTest() throws Exception {
+        // given
+        StudentScoreRepository studentScoreRepository = Mockito.mock(StudentScoreRepository.class);
+        StudentPassRepository studentPassRepository = Mockito.mock(StudentPassRepository.class);
+        StudentFailRepository studentFailRepository = Mockito.mock(StudentFailRepository.class);
+
+        String givenTestExam = "testexam";
+
+        StudentPass expected1 = StudentPass.builder().id(1L).studentName("jyujyu").exam(givenTestExam).avgScore(70.0).build();
+        StudentPass expected2 = StudentPass.builder().id(2L).studentName("griotold").exam(givenTestExam).avgScore(80.0).build();
+        StudentPass notExpected1 = StudentPass.builder().id(3L).studentName("iamnot").exam("secondexam").avgScore(90.0).build();
+
+        // studentPassRepository.findAll()을 수행할 때 아래 지정한 List 를 반환하라는 뜻
+        Mockito.when(studentPassRepository.findAll()).thenReturn(List.of(
+                expected1,
+                expected2,
+                notExpected1
+        ));
+
+        StudentScoreService studentScoreService = new StudentScoreService(studentScoreRepository,
+                studentPassRepository,
+                studentFailRepository);
+
+
+        // when
+        List<ExamPassStudentResponse> expectedResponses = List.of(expected1, expected2)
+                .stream()
+                .map(pass -> new ExamPassStudentResponse(pass.getStudentName(), pass.getAvgScore()))
+                .collect(Collectors.toList());
+
+        List<ExamPassStudentResponse> responses = studentScoreService.getPassStudents(givenTestExam);
+
+        responses.forEach((response) -> System.out.println(response.getStudentName() + "/" + response.getAvgScore()));
+
+        // then
+        // 파라미터로 전달한 givenTestExam 의 값들만 가져오는지를 검증
+//        assertIterableEquals(expectedResponses, responses);
+
+        // org.assertj.core.api.Assertions 로 검증하려면 아래를 사용
+        assertThat(responses).containsExactlyElementsOf(expectedResponses);
+    }
+
+    @Test
+    @DisplayName("불합격자 명단 가져오기 검증")
+    void getFailStudentsTest() throws Exception {
+        // given
+        StudentScoreRepository studentScoreRepository = Mockito.mock(StudentScoreRepository.class);
+        StudentPassRepository studentPassRepository = Mockito.mock(StudentPassRepository.class);
+        StudentFailRepository studentFailRepository = Mockito.mock(StudentFailRepository.class);
+
+        String givenTestExam = "testexam";
+
+        StudentFail expected1 = StudentFail.builder().id(1L).studentName("jyujyu").exam(givenTestExam).avgScore(30.0).build();
+        StudentFail expected2 = StudentFail.builder().id(2L).studentName("griotold").exam(givenTestExam).avgScore(40.0).build();
+        StudentFail notExpected1 = StudentFail.builder().id(3L).studentName("iamnot").exam("secondexam").avgScore(50.0).build();
+
+        // studentPassRepository.findAll()을 수행할 때 아래 지정한 List 를 반환하라는 뜻
+        Mockito.when(studentFailRepository.findAll()).thenReturn(List.of(
+                expected1,
+                expected2,
+                notExpected1
+        ));
+
+        StudentScoreService studentScoreService = new StudentScoreService(studentScoreRepository,
+                studentPassRepository,
+                studentFailRepository);
+
+
+        // when
+        List<ExamFailStudentResponse> expectedResponses = List.of(expected1, expected2)
+                .stream()
+                .map(pass -> new ExamFailStudentResponse(pass.getStudentName(), pass.getAvgScore()))
+                .collect(Collectors.toList());
+
+        List<ExamFailStudentResponse> responses = studentScoreService.getFailStudents(givenTestExam);
+
+        responses.forEach((response) -> System.out.println(response.getStudentName() + "/" + response.getAvgScore()));
+
+        // then
+        // 파라미터로 전달한 givenTestExam 의 값들만 가져오는지를 검증
+//        assertIterableEquals(expectedResponses, responses);
+
+        // org.assertj.core.api.Assertions 로 검증하려면 아래를 사용
+        assertThat(responses).containsExactlyElementsOf(expectedResponses);
+    }
 }
