74df1e921d683b78cf4ea4b11c442c67816ba8d5
Griotold <gotjd9773@naver.com>
2024-03-17 16:54:03 +0900

    간단한 성적 저장 애플리케이션 만들기 - ScoreApi - 서비스에서 개발한 코드를 호출

--- src/main/java/com/jyujyu/dayonetest/controller/ScoreApi.java
+++ src/main/java/com/jyujyu/dayonetest/controller/ScoreApi.java
@@ -3,29 +3,36 @@ package com.jyujyu.dayonetest.controller;
 import com.jyujyu.dayonetest.controller.request.SaveExamScoreRequest;
 import com.jyujyu.dayonetest.controller.response.ExamFailStudentResponse;
 import com.jyujyu.dayonetest.controller.response.ExamPassStudentResponse;
+import com.jyujyu.dayonetest.service.StudentScoreService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@RequiredArgsConstructor
 @RestController
 public class ScoreApi {
 
+    private final StudentScoreService studentScoreService;
     @PostMapping("/exam/{exam}/score")
-    public Object save(@PathVariable("exam") String exam,
+    public void save(@PathVariable("exam") String exam,
                        @RequestBody SaveExamScoreRequest request) {
-        return request;
+
+        studentScoreService.saveScore(
+                request.getStudentName(),
+                exam,
+                request.getKorScore(),
+                request.getEnglishScore(),
+                request.getMathScore());
     }
 
     @GetMapping("/exam/{exam}/pass")
     public List<ExamPassStudentResponse> pass(@PathVariable("exam") String exam) {
-        return List.of(
-                new ExamPassStudentResponse("jyujyu", 60.0)
-        );
+        return studentScoreService.getPassStudents(exam);
     }
 
     @GetMapping("/exam/{exam}/fail")
     public List<ExamFailStudentResponse> fail(@PathVariable("exam") String exam) {
-        return List.of(new ExamFailStudentResponse("jyujyu", 40.0));
+        return studentScoreService.getFailStudents(exam);
     }
 
 
